e13a1860ef5b559527bb0d364a679c4b934cf0b8
ripee <ripee.dev@gmail.com>
2019-07-18 13:32:31 +0200

    BoardCommonConfig.mk: Move BOARD_RIL_CLASS to device trees

--- BoardCommonConfig.mk
+++ BoardCommonConfig.mk
@@ -123,7 +123,6 @@ BOARD_MOBILEDATA_INTERFACE_NAME := "pdp0"
 BOARD_PROVIDES_LIBRIL := true
 BOARD_MODEM_TYPE := xmm6262
 TARGET_SPECIFIC_HEADER_PATH += device/samsung/smdk4412-common/include
-BOARD_RIL_CLASS := ../../../device/samsung/i9300/ril
 
 # Wifi
 BOARD_WLAN_DEVICE                := bcmdhd
