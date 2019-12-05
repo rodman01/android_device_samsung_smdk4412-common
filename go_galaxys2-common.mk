de39943fca459185e9ffa5d9cfffd5b5e3c1e8e1
Shilin Victor <chrono.monochrome@gmail.com>
2019-12-05 15:52:26 +0300

    smdk4x12: use speed ART filter
    Change-Id: Ie720af2bf36988dc5e0a879e6b8bf6f19bec04d9

--- go_galaxys2-common.mk
+++ go_galaxys2-common.mk
@@ -31,7 +31,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 
 # Speed profile services and wifi-service to reduce RAM and storage.
-PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed
 
 # Always preopt extracted APKs to prevent extracting out of the APK for gms
 # modules.
