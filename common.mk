f6ad0ac2f140777329556a478ae1297144305b74
Dominggoes Isakh <drjisakh@gmail.com>
2020-01-25 22:07:39 +0100

    smdk4412-common: Use Lineage's legacy Wifi interface

--- common.mk
+++ common.mk
@@ -203,7 +203,7 @@ PRODUCT_PACKAGES += \
 
 # Wifi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi@1.0-service.legacy \
     android.hardware.wifi.supplicant@1.0 \
     wificond \
     libwpa_client \
