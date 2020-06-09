c053e3ee7a2b2afbe6fa283f2bda04c18ca0061e
Dominggoes Isakh <drjisakh@gmail.com>
2020-06-09 23:41:06 +0200

    smdk4412-common: gps: Use android.hardware.gnss.service-exynos4
    Change-Id: I65cccaa1120b171aa7194a3d46a4886913b4d8c0

--- common.mk
+++ common.mk
@@ -117,6 +117,7 @@ PRODUCT_PACKAGES += \
     camera.device@1.0-impl-legacy \
     android.hardware.sensors@1.0-impl \
     android.hardware.gnss@1.0-impl \
+    android.hardware.gnss@1.0-service.exynos4 \
     libfimc \
     libfimg \
     libhwconverter \
